76f1d759c067a020a666edad2457f52da3036514
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-19 01:42:01 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,14 +1,15 @@
 class Dog
   attr_accessor :name, :breed, :age
-
-  @@all = []
-
-  def initalize(name, breed, age)
-    @name, @breed, @age = name, breed, age
-    @@all << self
+  DOGS = []
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    DOGS << self
   end
 
   def self.all
-    @@all
+    DOGS
   end
+
 end
